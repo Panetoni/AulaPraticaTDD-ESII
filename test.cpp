f909a05f08a062e8da146af6b7f8d14770a1da21
Panetoni <88630444+Panetoni@users.noreply.github.com>
2025-08-05 09:22:24 -0300

    2

--- test.cpp
+++ test.cpp
@@ -1,29 +1,37 @@
 #include <assert.h>
-#include <stdio.h>    
+#include <stdio.h>
 
 using namespace std;
 
 void assertEquals(int expected, int actual);
 
 class Dollar {
-    public:
-       int amount = 10;
-       Dollar(int amount) {}			
-       void times(int multiplier) {}
-    }	;
+public:
+    int amount;
+    Dollar(int amount) {
+        this->amount = amount;
+    }
+    Dollar* times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+};
 
 void testMultiplication() {
-       Dollar five = Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
-    }
+    Dollar* five = new Dollar(5);
+    Dollar* product = five->times(2);
+    assertEquals(10, product->amount);
+    product = five->times(3);
+    assertEquals(15, product->amount);
+    delete five;
+    delete product;
+}
 
 void assertEquals(int expected, int actual) {
-       if (expected != actual) {
-          printf("Expected %d but got %d\n", expected, actual);
-          throw "Test failed";
-       }
+    if (expected != actual) {
+        printf("Expected %d but got %d\n", expected, actual);
+        throw "Test failed";
     }
+}
 
 int main() {
     testMultiplication();
